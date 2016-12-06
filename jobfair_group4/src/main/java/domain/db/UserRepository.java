8b2614c98e4cb351af4fb4329b8a32e9fd7ba4e5
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-06 15:55:35 +0100

    edit

--- jobfair_group4/src/main/java/domain/db/UserRepository.java
+++ jobfair_group4/src/main/java/domain/db/UserRepository.java
@@ -39,7 +39,7 @@ public class UserRepository {
 		if (userID.isEmpty()) {
 			throw new DbException("Niets te vinden !");
 		}
-		String sql = "SELECT * " + "FROM jobfair_group4.spots" + " WHERE spotID = ?;";
+		String sql = "SELECT * FROM jobfair_group4.users WHERE userID = ?;";
 		User user = new User();
 		try {
 			statement = connection.prepareStatement(sql);
@@ -58,7 +58,44 @@ public class UserRepository {
 		}
 
 		return user;
+	}
 
+	public void add(User user) {
+		if (user == null) {
+			throw new DbException("Niets om toe te voegen.");
+		}
+		String sql = "SELECT * FROM jobfair_group4.users WHERE userID = ?";
+		try {
+			statement = connection.prepareStatement(sql);
+
+			statement.setString(1, user.getUserID());
+			
+			ResultSet result = statement.executeQuery();
+
+			if (result.next() && result.getString("userId").equals(user.getUserID())) {
+				throw new IllegalArgumentException("User already exists");
+			} else {
+				sql = "INSERT INTO \"r0589873_Web3\".person (userid, companyName, contactName, email, password, salt, firstName, lastName, role) "
+						+ "Values (?, ?, ?, ?, ?, ?, ?)";
+				try {
+					statement = connection.prepareStatement(sql);
+
+					statement.setString(1, user.getUserID());
+					statement.setString(2, user.getEmail());
+					statement.setString(3, user.getPassword());
+					statement.setString(4, user.getSalt());
+					statement.setString(5, user.getFirstName());
+					statement.setString(6, user.getLastName());
+					statement.setString(7, user.getRole().toString());
+
+					statement.executeUpdate();
+				} catch (SQLException e) {
+					throw new DbException(e.getMessage(), e);
+				}
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage(), e);
+		}
 	}
 
 	public Properties getProperties() {
