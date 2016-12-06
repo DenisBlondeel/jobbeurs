aa946ff449c34bb8d30d819ada074aca1be1d4ff
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-06 14:07:10 +0100

    Try to solve errors

--- jobfair_group4/src/main/java/domain/db/UserRepository.java
+++ jobfair_group4/src/main/java/domain/db/UserRepository.java
@@ -1,5 +1,22 @@
 package domain.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class UserRepository {
+
+	private PreparedStatement statement;
+	private Connection connection;
+	private Properties properties;
+
+	public void close() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		}
+	}
 
 }
