0874e96b1112218344a0b3e1a30a56366896bb4b
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-06 11:24:41 +0100

    add some content to RequestHandler

--- jobfair_group4/src/main/java/ui/controller/handler/RequestHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/RequestHandler.java
@@ -3,7 +3,11 @@ package ui.controller.handler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.service.SpotService;
+
 public abstract class RequestHandler {
+
+	private SpotService service;
 
 	public RequestHandler() {}
 
@@ -13,4 +17,11 @@ public abstract class RequestHandler {
 
 	public abstract void handleRequest(HttpServletRequest request, HttpServletResponse response);
 
+	public void setService(SpotService service) {
+		this.service = service;
+	}
+
+	protected SpotService getService() {
+		return service;
+	}
 }
