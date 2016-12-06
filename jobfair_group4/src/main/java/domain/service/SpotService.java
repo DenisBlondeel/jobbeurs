7d4e1a83b0ac169d96ed95c002fc9e8068727f13
Denis <Denis@10.25.136.25>
2016-12-06 13:10:43 +0100

    Service completed for spotRepository

--- jobfair_group4/src/main/java/domain/service/SpotService.java
+++ jobfair_group4/src/main/java/domain/service/SpotService.java
@@ -1,16 +1,61 @@
 package domain.service;
 
+import java.util.List;
 import java.util.Properties;
 
 import domain.db.SpotRepository;
+import domain.db.UserRepository;
+import domain.model.Spot;
+import domain.model.User;
 
 public class SpotService {
 	
 	private SpotRepository spotRepository;
+	private UserRepository userRepository;
 
 	public SpotService(Properties properties) {
 		
 		spotRepository = new SpotRepository(properties);
+		userRepository = new UserRepository();
 	}
+	
+	public Spot getSpot(String spotId)
+	{
+		return spotRepository.get(spotId);
+	}
+	
+	public List<Spot> getFreeSpot()
+	{
+		return spotRepository.getFreeSpots();
+	}
+	
+	public List<Spot> getOccupiedSpot()
+	{
+		return spotRepository.getOccupiedSpots();
+	}
+	
+	public List<Spot> getAlphabeticOccupiedSpot()
+	{
+		return spotRepository.getOccupiedSpots();
+	}
+	
+	public void addUserToSpot(String spotId, User user)
+	{
+		spotRepository.addUserToSpot(spotId, user);
+	}
+	
+	public void removeUserFromSpot(String spotId)
+	{
+		spotRepository.removeUserFromSpot(spotId);
+	}
+	
+	public SpotRepository getSpotRepository()
+	{
+		return spotRepository;
+	}
+	
+	
+	
+	
 
 }
