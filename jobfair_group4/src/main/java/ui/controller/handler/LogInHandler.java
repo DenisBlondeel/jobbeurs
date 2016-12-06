84f384331c06960facd5389c3048fb978edad65a
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-06 13:28:12 +0100

    Three Basic Handlers

---
+++ jobfair_group4/src/main/java/ui/controller/handler/LogInHandler.java
@@ -0,0 +1,18 @@
+package ui.controller.handler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogInHandler extends RequestHandler {
+
+	public LogInHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
