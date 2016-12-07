0b1ad02e6d743a886d60209ec4c355ea74bddb9d
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-07 15:41:53 +0100

    small edit

--- jobfair_group4/src/main/java/ui/controller/handler/LogInHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/LogInHandler.java
@@ -2,6 +2,7 @@ package ui.controller.handler;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,7 +12,7 @@ import domain.model.User;
 public class LogInHandler extends RequestHandler {
 
 	@Override
-	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String userID = request.getParameter("username");
 		String password = request.getParameter("password");
 		User user = this.getService().getUserIfAuthenticated(userID, password);
@@ -20,9 +21,11 @@ public class LogInHandler extends RequestHandler {
 			HttpSession session = request.getSession();
 			session.setAttribute("user", user);
 			request.setAttribute("userid", user.getUserID());
+			response.sendRedirect("Controller");
+		} else {
+			request.getRequestDispatcher("login.jsp").forward(request, response);
 		}
 
-		response.sendRedirect("Controller");
 	}
 
 }
