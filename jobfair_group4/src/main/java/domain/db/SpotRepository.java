5011b36f1b21fc140448acca14318e7e7dcb9d6f
Denis <Denis@10.25.136.25>
2016-12-07 16:54:42 +0100

    Authentication vliegt eruit

--- jobfair_group4/src/main/java/domain/db/SpotRepository.java
+++ jobfair_group4/src/main/java/domain/db/SpotRepository.java
@@ -49,14 +49,6 @@ public class SpotRepository {
 			statement = connection.prepareStatement(sql);
 			statement.setString(1, spotID);
 			ResultSet results = statement.executeQuery();
-<<<<<<< HEAD
-			results.next();
-			spot.setAmountTables(results.getInt("amountTables"));
-			spot.setAmountChairs(results.getInt("amountChairs"));
-			spot.setElectricity(results.getBoolean("electricity"));
-			spot.setRemarks(results.getString("remarks"));
-			spot.setUserID(results.getString("userid"));
-=======
 			if(results.next()){
 				spot.setAmountTables(results.getInt("amountTables"));
 				spot.setAmountChairs(results.getInt("amountChairs"));
@@ -66,7 +58,6 @@ public class SpotRepository {
 				System.out.println("Get your shit together Denis");
 			}
 			
->>>>>>> branch 'master' of https://github.com/elke-steegmans/jobfair_group4.git
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage(), e);
 		}
