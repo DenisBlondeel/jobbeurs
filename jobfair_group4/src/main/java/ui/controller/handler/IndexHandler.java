feed97750b38e2b31342c7fa80cb37b1deaed93a
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-07 15:16:52 +0100

    push indexHandler

--- jobfair_group4/src/main/java/ui/controller/handler/IndexHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/IndexHandler.java
@@ -5,12 +5,23 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.model.User;
 
 public class IndexHandler extends RequestHandler {
 
 	@Override
 	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+
+		if (user!=null) {
+			request.setAttribute("userid", user.getUserID());
+		}
+
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 }
