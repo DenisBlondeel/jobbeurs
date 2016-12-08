db811834d8d1386f79fbf99f1bfdc480c4dfc804
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-08 21:40:32 +0100

    Alert user when spot is canceled

--- jobfair_group4/src/main/java/ui/controller/handler/ConfirmDeleteHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/ConfirmDeleteHandler.java
@@ -2,9 +2,15 @@ package ui.controller.handler;
 
 import java.io.IOException;
 
+import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.model.EmailSender;
+import domain.model.Spot;
+import domain.model.User;
 
 public class ConfirmDeleteHandler extends RequestHandler{
 
@@ -13,12 +19,22 @@ public class ConfirmDeleteHandler extends RequestHandler{
 			throws ServletException, IOException {
 		String spotnr = request.getParameter("spotnr");
 		String action = request.getParameter("submit");
+		Spot spot = this.getService().getSpot(spotnr);
+
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+
 		if(action.equals("ja")){
 			service.removeUserFromSpot(spotnr);
 			request.setAttribute("annuleer", "annuleer");
+			try {
+				new EmailSender().sendCancelationMail(spot, user.getEmail());
+			} catch (MessagingException e) {
+				
+			}
 			response.sendRedirect("Controller?action=");
 		} else {
-			request.getRequestDispatcher("myspot.jsp").forward(request, response);
+			request.getRequestDispatcher("Controller?action=myspot").forward(request, response);
 		}
 	}
 
