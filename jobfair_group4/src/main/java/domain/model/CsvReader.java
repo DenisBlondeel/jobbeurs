947c30b09a167dfe832c60a5928d2c282b761a79
Bram <Bram@Bram-PC>
2016-12-08 11:53:30 +0100

    Mama, life had just begun

--- jobfair_group4/src/main/java/domain/model/CsvReader.java
+++ jobfair_group4/src/main/java/domain/model/CsvReader.java
@@ -1,7 +1,5 @@
 package domain.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,14 +7,11 @@ import java.util.Scanner;
 
 public class CsvReader {
 	
-	private BufferedReader br = null;
-	private String line = "";
-	
-	
 	public List<User> read(InputStream fileContent){
 		List<User> users = new ArrayList<User>();
 		Scanner inputStream = new Scanner(fileContent);
-		while(inputStream.hasNext()){
+		while(inputStream.hasNextLine()){
+			String line = inputStream.nextLine();
 			String[] data = line.split(";");
 			User user = new User();
 			String companyName = data[0];
@@ -29,6 +24,7 @@ public class CsvReader {
 			user.generatePassword();
 			users.add(user);
 		}
+		inputStream.close();
 		return users;
 	}
 
