f4c04b704d2d1aebcbdc173fda727caa88d55c12
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-08 10:41:38 +0100

    Is this the real life?

--- jobfair_group4/src/test/java/view/EmailTest.java
+++ jobfair_group4/src/test/java/view/EmailTest.java
@@ -1,5 +1,22 @@
 package view;
 
-public class SpotRepositroyTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import domain.model.EmailSender;
+
+public class EmailTest {
+
+	EmailSender sender;
+	
+
+	@Before
+	public void setUp() {
+		sender = new EmailSender();
+	}
+
+	@Test
+	public void sendConfirmationMailTest() {
+		sender.sendConfirmationMail("Ik hoop dat dit werkt", "brechtdecuyper@hotmail.com");
+	}
 }
