bf3c83cdd8acab25a8b19b25356d21d0913e661c
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-08 21:44:00 +0100

    schrijffout :/

--- jobfair_group4/src/main/java/domain/model/EmailSender.java
+++ jobfair_group4/src/main/java/domain/model/EmailSender.java
@@ -78,7 +78,7 @@ public class EmailSender {
 	public void sendCancelationMail(Spot spot, String emailreceiver) throws MessagingException {
 		String subject = "Jobbeurs 2017 - UCLL Leuven: Annulatie plaats";
 		String message = "Beste,<br><br>Uw plaats met nummer " + spot.getSpotID() + " werd geannuleerd.<br>"
-				+ "Indien u deze mail krijgt zonder op de hoogte te zijn van deze veranderingen, gelieve dan contact op"
+				+ "Indien u deze mail krijgt zonder op de hoogte te zijn van deze veranderingen, gelieve dan contact op "
 				+ "te nemen met onze verantwoordelijke"
 				+ "<br>Tot binnenkort.<br><br>"
 				+ "--<br>"
@@ -90,7 +90,7 @@ public class EmailSender {
 	public void sendAlmostSoldOutMail(List<String> emailreceivers) throws MessagingException {
 		String subject = "Jobbeurs 2017 - UCLL Leuven: Bijna volzet";
 		String message = "Melding voor de administrator: De te huren locaties zijn bijna volzet, er zijn minder dan 10 plaatsen nog vrij.<br>"
-				+ "Het is dus ongeveer tijd geworden om meer standplaatsen toe te voegen zodat meer bedrijven"
+				+ "Het is dus ongeveer tijd geworden om meer standplaatsen toe te voegen zodat meer bedrijven "
 				+ "kunnen strijden voor een plaatsje.";
 		for (String emailreceiver : emailreceivers) {
 			sendFromGMail(subject, message, emailreceiver);
@@ -101,10 +101,10 @@ public class EmailSender {
 		String subject = "Jobbeurs 2017 - UCLL Leuven: De einddatum nadert";
 		String message = "Beste,<br><br>Vanaf " + new SimpleDateFormat("EEEE").format(date) + " "
 				+ new SimpleDateFormat("dd").format(date) + " "
-				+ new SimpleDateFormat("MMMM").format(date) + " eindigt de mogelijkheid"
-				+ "om zelf je plaats je kiezen op onze jobbeurs."
+				+ new SimpleDateFormat("MMMM").format(date) + " eindigt de mogelijkheid "
+				+ "om zelf je plaats je kiezen op onze jobbeurs. "
 				+ "U heeft dus nog even tijd om uw keuze te maken.<br>"
-				+ "Moest u uw keuze niet tijdig gemaakt hebben, zal onze verantwoordelijke"
+				+ "Moest u uw keuze niet tijdig gemaakt hebben, zal onze verantwoordelijke "
 				+ "een willekeurige spot in uw plaats kiezen."
 				+ "<br>Tot binnenkort.<br><br>"
 				+ "--<br>"
