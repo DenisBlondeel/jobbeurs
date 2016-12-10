6da23233bc7f3a06298fcd8b69a9f9d153ab3635
Bram <Bram@Bram-PC>
2016-12-10 16:41:28 +0100

    csv download fix

--- jobfair_group4/src/main/java/ui/controller/handler/DownloadHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/DownloadHandler.java
@@ -1,47 +1,57 @@
 package ui.controller.handler;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import domain.model.CsvWriter;
 import domain.model.User;
 
 public class DownloadHandler extends RequestHandler{
 	
-	private static final int BYTES_DOWNLOAD = 1024;
+	private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
 	@Override
 	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		List<User> users = service.getAllCompanies();
-		CsvWriter writer = new CsvWriter();
-		File csv = writer.write(users);
-		String file = csv.getPath();
 		
 		response.setContentType("text/csv");
-		response.setHeader("Content-Disposition",
-	                     "attachment;filename=Inschrijvingen_jobbeurs_"+writer.getYear());
-		ServletContext ctx = request.getServletContext();
-		InputStream is = ctx.getResourceAsStream(file);
+        String reportName =  "Inschrijvingen_Jobbeurs_"+getYear()+".csv";
+        response.setHeader("Content-disposition", "attachment; " +
+                "filename=" + reportName);   
 
-		int read=0;
-		byte[] bytes = new byte[BYTES_DOWNLOAD];
-		OutputStream os = response.getOutputStream();
+        ArrayList<String> rows = new ArrayList<String>();
+        List<User> users = service.getAllCompanies();
 
-		while((read = is.read(bytes))!= -1){
-			os.write(bytes, 0, read);
-		}
-		os.flush();
-		os.close();
+        for (User user : users) {
+            rows.add(user.getCompanyName()+";"+user.getContactName()+";;"+user.getEmail()+"\n");
+        }
+
+        Iterator<String> iter = rows.iterator();
+        while (iter.hasNext()){
+            String outputString = (String) iter.next();
+            response.getOutputStream().print(outputString);
+        }
+
+        response.getOutputStream().flush();
 		
 		request.getRequestDispatcher("Controller?action=admin").forward(request, response);
+	}
+
+	private String getYear() {
+		Date date = new Date();
+		String currentDate = sdf.format(date);
+		String year = "";
+		for(int i=0; i<4; i++){
+			year += currentDate.charAt(i);
+		}
+		return year;
 	}
 }
