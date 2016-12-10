6a170e52049d71022c88bc6c19db5d559e0aa289
Bram <Bram@Bram-PC>
2016-12-10 17:15:31 +0100

    fix voor nullpointer bij update (hopelijk)

--- jobfair_group4/src/main/java/ui/controller/handler/UpdateHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/UpdateHandler.java
@@ -17,38 +17,47 @@ public class UpdateHandler extends RequestHandler{
 		String spotnr = request.getParameter("spotnr");
 		Spot spot = service.getSpot(spotnr);
 		switch(spot.getAmountChairs()){
-		case 0:
-			request.setAttribute("ch0", checked);
-			request.setAttribute("ch1", "");
-			request.setAttribute("ch2", "");
-			break;
-		case 1:
-			request.setAttribute("ch0", "");
-			request.setAttribute("ch1", checked);
-			request.setAttribute("ch2", "");
-			break;
-		case 2:
-			request.setAttribute("ch0", "");
-			request.setAttribute("ch1", "");
-			request.setAttribute("ch2", checked);
-			break;
-		default:
-			request.setAttribute("ch0", "");
-			request.setAttribute("ch1", "");
-			request.setAttribute("ch2", checked);
+			case 0:
+				request.setAttribute("ch0", checked);
+				request.setAttribute("ch1", "");
+				request.setAttribute("ch2", "");
+				break;
+			case 1:
+				request.setAttribute("ch0", "");
+				request.setAttribute("ch1", checked);
+				request.setAttribute("ch2", "");
+				break;
+			case 2:
+				request.setAttribute("ch0", "");
+				request.setAttribute("ch1", "");
+				request.setAttribute("ch2", checked);
+				break;
+			default:
+				request.setAttribute("ch0", "");
+				request.setAttribute("ch1", "");
+				request.setAttribute("ch2", checked);
 		}
-		if(spot.getAmountTables() == 0){
-			request.setAttribute("tb0", checked);
-			request.setAttribute("tb1", "");
-		} else {
-			request.setAttribute("tb0", "");
-			request.setAttribute("tb1", checked);
+		
+		switch(spot.getAmountTables()){
+			case 0:
+				request.setAttribute("tb0", checked);
+				request.setAttribute("tb1", "");
+				break;
+			case 1:
+				request.setAttribute("tb0", "");
+				request.setAttribute("tb1", checked);
+				break;
+			default:
+				request.setAttribute("tb0", checked);
+				request.setAttribute("tb1", "");
 		}
+		
 		if(spot.getElectricity()){
 			request.setAttribute("el", checked);
 		} else {
 			request.setAttribute("el", "");
 		}
+		
 		request.setAttribute("userid", spot.getUserID());
 		request.setAttribute("extra", spot.getRemarks());
 		request.setAttribute("spotnr", spotnr);
