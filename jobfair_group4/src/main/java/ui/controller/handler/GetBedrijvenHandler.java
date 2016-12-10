2314201886aee197a7ff8fe6ae24c81cd092abd2
Denis <Denis@DESKTOP-KBKV3K3.home>
2016-12-10 17:48:48 +0100

    Started implementing delete company

---
+++ jobfair_group4/src/main/java/ui/controller/handler/GetBedrijvenHandler.java
@@ -0,0 +1,26 @@
+package ui.controller.handler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.model.RoleEnum;
+
+public class GetBedrijvenHandler extends RequestHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException
+	{
+		request.setAttribute("bedrijven", service.getAllUsers());
+		request.getRequestDispatcher("deleteBedrijf.jsp").forward(request, response);
+	}
+
+	@Override
+	public RoleEnum[] getAccesList() {
+		return new RoleEnum[]{RoleEnum.ADMIN};
+	}
+
+}
