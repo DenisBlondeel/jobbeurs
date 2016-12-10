f5ec288271c8f99e6811f3116b71560f23493b9b
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-10 13:57:24 +0100

    admin mag niet op een spot kunnen klikken en zelf een plaats reserveren

--- jobfair_group4/src/main/java/ui/controller/handler/ShowOptionsHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/ShowOptionsHandler.java
@@ -44,8 +44,8 @@ public class ShowOptionsHandler extends RequestHandler {
 	@Override
 	public RoleEnum[] getAccesList() {
 		if (this.getTimeHasCome()) {
-			return new RoleEnum[]{RoleEnum.ADMIN};
+			return new RoleEnum[]{};
 		}
-		return new RoleEnum[]{RoleEnum.COMPANY, RoleEnum.ADMIN};
+		return new RoleEnum[]{RoleEnum.COMPANY};
 	}
 }
--- jobfair_group4/src/main/java/ui/controller/handler/SpotOptionsHandler.java
+++ jobfair_group4/src/main/java/ui/controller/handler/SpotOptionsHandler.java
@@ -61,8 +61,8 @@ public class SpotOptionsHandler extends RequestHandler {
 	@Override
 	public RoleEnum[] getAccesList() {
 		if (this.getTimeHasCome()) {
-			return new RoleEnum[]{RoleEnum.ADMIN};
+			return new RoleEnum[]{};
 		}
-		return new RoleEnum[]{RoleEnum.COMPANY, RoleEnum.ADMIN};
+		return new RoleEnum[]{RoleEnum.COMPANY};
 	}
 }
