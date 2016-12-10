0f53f23b4f187d2d72eb9de6f7e8400c60a2ab3c
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-10 15:10:51 +0100

    handler classes

---
+++ jobfair_group4/src/main/java/ui/controller/handler/GetAdminHandler.java
@@ -0,0 +1,18 @@
+package ui.controller.handler;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetAdminHandler extends RequestHandler {
+
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("admins", getService().getAdmins());
+		request.getRequestDispatcher("deleteAdmin.jsp").forward(request, response);
+	}
+
+}
