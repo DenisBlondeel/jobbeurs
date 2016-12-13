5ddc35bba256b6544c3ecf03198040584295a8d8
Brecht Decuyper <brecht.decuyper@student.kuleuven.be>
2016-12-13 09:11:17 +0100

    fast csv parser

--- jobfair_group4/src/main/java/domain/model/CsvReader.java
+++ jobfair_group4/src/main/java/domain/model/CsvReader.java
@@ -5,33 +5,81 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 import javax.mail.MessagingException;
+
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
 
 import domain.service.Service;
 
 public class CsvReader {
 
 	private EmailSender emailSender = new EmailSender();
+	private CsvParserSettings settings;
+	private CsvParser parser;
 	
+	public CsvReader() {
+		settings = new CsvParserSettings();
+		settings.getFormat().setDelimiter(';');
+		settings.getFormat().setLineSeparator("\n");
+		settings.selectIndexes(0,1,2);
+		parser = new CsvParser(settings);
+	}
+	
+//	public void read(InputStream in, Service service) throws MessagingException{
+//		List<User> users = new ArrayList<>();
+//		Map<User, String> mailList = new HashMap<>();
+//
+//		Scanner inputStream;
+//		inputStream = new Scanner(in, "ISO-8859-1");
+//		inputStream.nextLine();
+//		while(inputStream.hasNextLine()){
+//			String line = inputStream.nextLine();
+//			String[] data = line.split(";");
+//			User user = new User();
+//			String companyName = data[0];
+//			String contactName = data[1];
+//			String email = data[2];
+//			if(companyName.equals("Bedrijfsnaam") || contactName.equals("Naam contactpersoon") || email.equals("E-mailadres contactpersoon")){
+//				continue;
+//			}
+//			user.setCompanyName(companyName);
+//			user.setContactName(contactName);
+//			user.setEmail(email);
+//			user.generateUserId(companyName);
+//			String tempPass = user.generatePassword();
+//
+//			users.add(user);
+//			mailList.put(user, tempPass);
+//		}
+//		inputStream.close();
+//		
+//		service.addUsers(users);
+//
+//		try {
+//			emailSender.sendNewCompanyMail(mailList);
+//		} catch (MessagingException e) {
+//			throw new MessagingException(e.getMessage(), e);
+//		}
+//	}
+
 	public void read(InputStream in, Service service) throws MessagingException{
 		List<User> users = new ArrayList<>();
 		Map<User, String> mailList = new HashMap<>();
 
-		Scanner inputStream;
-		inputStream = new Scanner(in, "ISO-8859-1");
-		inputStream.nextLine();
-		while(inputStream.hasNextLine()){
-			String line = inputStream.nextLine();
-			String[] data = line.split(";");
+		List<String[]> allRows = parser.parseAll(in, "ISO-8859-1");
+		System.out.println(allRows.size());
+		for (int i = 0; i < allRows.size(); i++) {
+			String[] data = allRows.get(i);
 			User user = new User();
 			String companyName = data[0];
 			String contactName = data[1];
 			String email = data[2];
-//			if(companyName.equals("Bedrijfsnaam") || contactName.equals("Naam contactpersoon") || email.equals("E-mailadres contactpersoon")){
-//				continue;
-//			}
+			if(companyName.equals("Bedrijfsnaam") || contactName.equals("Naam contactpersoon") || email.equals("E-mailadres contactpersoon")){
+				continue;
+			}
 			user.setCompanyName(companyName);
 			user.setContactName(contactName);
 			user.setEmail(email);
@@ -41,8 +89,6 @@ public class CsvReader {
 			users.add(user);
 			mailList.put(user, tempPass);
 		}
-		inputStream.close();
-		
 		service.addUsers(users);
 
 		try {
