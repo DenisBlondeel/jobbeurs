5365647b4f7d9e6e358291e941889cf5044e97bf
Bram <Bram@Bram-PC>
2016-12-13 01:01:46 +0100

    bugfix csvreader

--- jobfair_group4/src/main/java/domain/model/CsvReader.java
+++ jobfair_group4/src/main/java/domain/model/CsvReader.java
@@ -27,7 +27,7 @@ public class CsvReader {
 			String companyName = data[0];
 			String contactName = data[1];
 			String email = data[2];
-			if(!companyName.equals("bedrijf") && !contactName.equals("contactpersoon") && !email.equals("email")){
+			if(!companyName.equals("Bedrijfsnaam") && !contactName.equals("Naam contactpersoon") && !email.equals("Email contactpersoon")){
 				user.setCompanyName(companyName);
 				user.setContactName(contactName);
 				user.setEmail(email);
